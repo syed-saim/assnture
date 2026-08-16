00dbc25fdb115a2299d136a991ae09b486e7242c
sidaly <anaqvi@ainsolutions.ai>
2026-08-16 15:40:56 +0400

    minor fixes

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,7 +2,6 @@ import { Hero } from './Hero'
 import { PartnersMarquee } from './PartnersMarquee'
 import { RwaUseCasesSection } from './RwaUseCasesSection'
 import { MarketsSection } from './MarketsSection'
-import { StatsSection } from './StatsSection'
 import { ContactSection } from './ContactSection'
 
 export function HomePage() {
