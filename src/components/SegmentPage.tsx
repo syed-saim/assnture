efdccc4ee604796fb6019298a393dd779d5df477
Syed Saim Ali <68299784+syed-saim@users.noreply.github.com>
2026-08-17 01:45:02 +0500

    redeployment fixes

--- src/components/SegmentPage.tsx
+++ src/components/SegmentPage.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
 import type { SegmentPage as SegmentPageData } from '../data/pages'
 import { ContactSection } from './ContactSection'
